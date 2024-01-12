294485b85b0dc33c89ddf5cb7c2b28cf8d9f6406
박현우 <ghwhsbsb123@gmail.com>
2024-01-13 01:54:38 +0900

    Particle trigger.

--- Assets/Scripts/Enemy1.cs
+++ Assets/Scripts/Enemy1.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float maxHealth = 100f;
+    public float MaxHealth = 100f;
     private float currentHealth;
     private bool isDead = false;
     private EnemyState currentState;
 
-    public float detectionRange = 100f;
-    public float combatRange = 30f;
-    public float moveSpeed = 2f; // 적의 이동 속도
-    public Transform player;
+    public float DetectionRange = 100f;
+    public float CombatRange = 30f;
+    public float MoveSpeed = 2f; // 적의 이동 속도
+    public Transform Player;
 
     private Rigidbody rb;
-    //파티클 관리
-    private ParticleSystem particleSystem;
-    
+    private ParticleSystem ps;
+
     private enum EnemyState
     {
         Dormant,
@@ -23,41 +22,31 @@ public class Enemy : MonoBehaviour
         Dead
     }
 
-    void Start()
+    void Awake()
     {
-        currentHealth = maxHealth;
+        currentHealth = MaxHealth;
         currentState = EnemyState.Dormant;
         rb = GetComponent<Rigidbody>();
-        combatRange = 30f;
-
-        // 플레이어를 이름으로 찾아서 할당
-        player = GameObject.Find("Player").transform;
-
-        if (player == null)
-        {
-            Debug.LogError("Player not found. Make sure the Player object has the correct name.");
-        }
-
-        // 파티클 시스템 컴포넌트 가져오기
-        particleSystem = GetComponent<ParticleSystem>();
+        ps = GetComponent<ParticleSystem>();
 
         // 파티클 좌표를 월드 좌표 공간으로 설정
         SetSimulationSpaceToWorld();
     }
+
     void SetSimulationSpaceToWorld()
     {
-        var mainModule = particleSystem.main;
+        var mainModule = ps.main;
         mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
     }
-    
+
     void Update()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.position);
 
         switch (currentState)
         {
             case EnemyState.Dormant:
-                if (distanceToPlayer < detectionRange)
+                if (distanceToPlayer < DetectionRange)
                 {
                     currentState = EnemyState.Combat;
                     Debug.Log("Combat mode activated!");
@@ -65,7 +54,7 @@ public class Enemy : MonoBehaviour
                 break;
 
             case EnemyState.Combat:
-                if (distanceToPlayer > detectionRange)
+                if (distanceToPlayer > DetectionRange)
                 {
                     currentState = EnemyState.Dormant;
                     Debug.Log("Returning to dormant state.");
@@ -73,18 +62,21 @@ public class Enemy : MonoBehaviour
                 else
                 {
                     //print(distanceToPlayer + " ||  " + combatRange);
-                    Vector3 direction = player.position - transform.position;
+                    Vector3 direction = Player.position - transform.position;
                     // 플레이어를 향해 회전
                     /*Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
                     transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);*/
 
                     // 플레이어 쪽으로 Rigidbody의 velocity를 조절하여 이동
-                    if(distanceToPlayer > combatRange) {
-                        rb.velocity = direction * moveSpeed;
-                    } else {
+                    if (distanceToPlayer > CombatRange)
+                    {
+                        rb.velocity = direction * MoveSpeed;
+                    }
+                    else
+                    {
                         rb.velocity *= 0.5f;
                     }
-                        
+
                 }
                 break;
 
@@ -113,5 +105,10 @@ public class Enemy : MonoBehaviour
 
         // 여기에는 사망 시 수행할 동작을 추가
         Destroy(gameObject, 3f);
+    }
+
+    void OnParticleTrigger()
+    {
+        Debug.Log("Triggered");
     }
 }
